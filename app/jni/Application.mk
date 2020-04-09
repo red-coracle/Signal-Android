966752dfb54c59b64a08ed85374c7bc24cc61254
red-coracle <red-coracle@users.noreply.github.com>
2020-04-09 15:27:21 -0400

    Update gradle

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,6 +1,6 @@
 # Built with NDK 19.2.5345600
 APP_PLATFORM := android-19
-APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := arm64-v8a
 APP_STL := c++_static
 APP_CPPFLAGS += -fexceptions
 APP_OPTIM := release
